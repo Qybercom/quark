fc99a3a4a75670594c7b4f2bf7bacb19e7ccb659
Alex Furnica <saniafd93@gmail.com>
2015-01-16 20:10:15 +0200

    patched core

--- Quark.php
+++ Quark.php
@@ -1845,7 +1845,7 @@ class QuarkModel {
 	 * @return IQuarkModel
 	 */
 	private static function _export (IQuarkModel $model, $options = []) {
-		$output = new $model();
+		//$output = new $model();
 		$fields = $model->Fields();
 
 		if (!isset($options[self::OPTION_VALIDATE]))
@@ -1853,18 +1853,18 @@ class QuarkModel {
 
 		if ($options[self::OPTION_VALIDATE] && !self::_validate($model)) return false;
 
-		foreach ($model as $key => $value) {
+		foreach ($model as $key => &$value) {
 			if (!Quark::PropertyExists($fields, $key) && $model instanceof IQuarkStrongModel) continue;
 
 			if ($value instanceof QuarkCollection) {
-				$output->$key = $value->Collection(function ($item) {
+				$model->$key = $value->Collection(function ($item) {
 					return self::_unlink($item);
 				});
 			}
-			else $output->$key = self::_unlink($value);
+			else $model->$key = self::_unlink($value);
 		}
 
-		return self::_normalize($output);
+		return self::_normalize($model);
 	}
 
 	/**
