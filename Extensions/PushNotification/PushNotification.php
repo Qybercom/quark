0a38590f357e96e8117796fc1b7e9d3d1ef01e96
Alex Furnica <saniafd93@gmail.com>
2015-01-19 16:26:33 +0200

    patched core

--- Extensions/PushNotification/PushNotification.php
+++ Extensions/PushNotification/PushNotification.php
@@ -102,7 +102,7 @@ class PushNotification implements IQuarkExtension {
 				$status = $this->_client->Response()->Status()->code == 200;
 
 				if (!$status)
-					Quark::Log("Push notification failed. Response:\r\n" . print_r($response, true), Quark::LOG_WARN);
+					Quark::Log("Push notification failed. Response:\r\n" . print_r($this->_client, true), Quark::LOG_WARN);
 
 				$ok &= $status;
 			}
