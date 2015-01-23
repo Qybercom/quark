65fc6aed51adb8fb182ac273c81dea83e5bb1f6c
Alex Furnica <saniafd93@gmail.com>
2015-01-23 23:15:01 +0200

    patched core

--- Quark.php
+++ Quark.php
@@ -495,28 +495,19 @@ class Quark {
 }
 
 spl_autoload_extensions('.php');
+
 spl_autoload_register(function ($class) {
-	$safe = Quark::GuID();//sha1($class);
+	$file = Quark::NormalizePath(__DIR__ . '/' . substr($class, 6) . '.php', false);
 
-	$class = str_replace($safe , '\\Quark\\', str_replace('Quark\\', '', str_replace('\\Quark\\', $safe, $class)));
+	if (is_file($file))
+		include_once $file;
+});
 
-	$quark = Quark::NormalizePath(__DIR__ . '/' . $class . '.php', false);
-	$app = Quark::NormalizePath(Quark::Host() . '/' . $class . '.php', false);
+spl_autoload_register(function ($class) {
+	$file = Quark::NormalizePath(Quark::Host() . '/' . $class . '.php', false);
 
-	$file = $quark;
-
-	if (!is_file($quark)) {
-		if (!is_file($app))
-			throw new QuarkArchException(
-				'Class file ' . $app . ' is invalid class path.' . "\r\n" .
-				' - Quark: [' . print_r($quark, true) . ']' . "\r\n" .
-				' - App: [' . print_r($app, true) . ']'
-			);
-
-		$file = $app;
-	}
-
-	include_once $file;
+	if (is_file($file))
+		include_once $file;
 });
 
 /**
