236a91981ad9440343e7b298e5777e73ee88d127
Alex Furnica <saniafd93@gmail.com>
2015-04-14 14:21:15 +0300

    patched TokenChargeScenario

--- Extensions/Payment/Providers/CloudPayments/PaymentScenarios/TokenChargeScenario.php
+++ Extensions/Payment/Providers/CloudPayments/PaymentScenarios/TokenChargeScenario.php
@@ -63,7 +63,7 @@ class TokenChargeScenario implements IQuarkPaymentScenario {
 		$this->_response = $http->Action();
 
 		if (!isset($this->_response->Success) || !$this->_response->Success) {
-			Quark::Log(print_r($this, true));
+			Quark::Log(print_r($http, true));
 			return false;
 		}
 
