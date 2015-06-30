4c07b4d5438388f4f559a451e1c39d3ab40c1ea9
Alex Furnica <saniafd93@gmail.com>
2015-06-30 14:01:53 +0300

    update

--- Quark.php
+++ Quark.php
@@ -587,6 +587,8 @@ class QuarkFPMEnvironmentProvider implements IQuarkThread {
 	 * @return mixed
 	 */
 	public function Thread () {
+		ob_start(null, 0, PHP_OUTPUT_HANDLER_STDFLAGS ^ PHP_OUTPUT_HANDLER_REMOVABLE);
+
 		$service = new QuarkService(
 			$_SERVER['REQUEST_URI'],
 			Quark::Config()->Processor(QuarkConfig::REQUEST),
