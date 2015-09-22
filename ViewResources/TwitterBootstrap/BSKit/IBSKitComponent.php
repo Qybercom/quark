0aff21b472f6afacc277f7c42d90093a1ebcef17
Alex Furnica <saniafd93@gmail.com>
2015-09-22 22:19:09 +0300

    update

---
+++ ViewResources/TwitterBootstrap/BSKit/IBSKitComponent.php
@@ -0,0 +1,14 @@
+<?php
+namespace Quark\ViewResources\TwitterBootstrap\BSKit;
+
+/**
+ * Interface IBSKitComponent
+ *
+ * @package Quark\ViewResources\TwitterBootstrap\BSKit
+ */
+interface IBSKitComponent {
+	/**
+	 * @return string
+	 */
+	public function BSKitComponentName();
+}
