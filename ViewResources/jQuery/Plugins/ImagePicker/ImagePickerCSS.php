dd6aa277e207b8c1af00e70ee11af9f7f6721526
Alex Furnica <saniafd93@gmail.com>
2015-12-28 20:20:02 +0200

    patched ImagePicker

--- ViewResources/jQuery/Plugins/ImagePicker/ImagePickerCSS.php
+++ ViewResources/jQuery/Plugins/ImagePicker/ImagePickerCSS.php
@@ -1,26 +1,24 @@
 <?php
 namespace Quark\ViewResources\jQuery\Plugins\ImagePicker;
 
-use Quark\IQuarkForeignViewResource;
 use Quark\IQuarkViewResource;
 use Quark\IQuarkViewResourceType;
 
 use Quark\QuarkCSSViewResourceType;
-use Quark\QuarkDTO;
 
 /**
  * Class ImagePickerCSS
  *
  * @package Quark\ViewResources\jQuery\Plugins\ImagePicker
  */
-class ImagePickerCSS implements IQuarkViewResource, IQuarkForeignViewResource {
+class ImagePickerCSS implements IQuarkViewResource {
 	/**
 	 * @var string $_version = ImagePicker::CURRENT_VERSION
 	 */
 	private $_version = ImagePicker::CURRENT_VERSION;
 
 	/**
-	 * @param string $version = TwitterBootstrap::CURRENT_VERSION
+	 * @param string $version = ImagePicker::CURRENT_VERSION
 	 */
 	public function __construct ($version = ImagePicker::CURRENT_VERSION) {
 		$this->_version = $version;
@@ -38,12 +36,5 @@ class ImagePickerCSS implements IQuarkViewResource, IQuarkForeignViewResource {
 	 */
 	public function Location () {
 		return 'https://cdnjs.cloudflare.com/ajax/libs/image-picker/' . $this->_version . '/image-picker.min.css';
-	}
-
-	/**
-	 * @return QuarkDTO
-	 */
-	public function RequestDTO () {
-		// TODO: Implement RequestDTO() method.
 	}
 }
--- ViewResources/jQuery/Plugins/ImagePicker/ImagePickerJS.php
+++ ViewResources/jQuery/Plugins/ImagePicker/ImagePickerJS.php
@@ -1,12 +1,10 @@
 <?php
 namespace Quark\ViewResources\jQuery\Plugins\ImagePicker;
 
-use Quark\IQuarkForeignViewResource;
 use Quark\IQuarkViewResource;
 use Quark\IQuarkViewResourceType;
 use Quark\IQuarkViewResourceWithDependencies;
 
-use Quark\QuarkDTO;
 use Quark\QuarkJSViewResourceType;
 
 use Quark\ViewResources\jQuery\jQueryCore;
@@ -16,14 +14,14 @@ use Quark\ViewResources\jQuery\jQueryCore;
  *
  * @package Quark\ViewResources\jQuery\Plugins\ImagePicker
  */
-class ImagePickerJS implements IQuarkViewResource, IQuarkForeignViewResource, IQuarkViewResourceWithDependencies {
+class ImagePickerJS implements IQuarkViewResource, IQuarkViewResourceWithDependencies {
 	/**
 	 * @var string $_version = ImagePicker::CURRENT_VERSION
 	 */
 	private $_version = ImagePicker::CURRENT_VERSION;
 
 	/**
-	 * @param string $version = TwitterBootstrap::CURRENT_VERSION
+	 * @param string $version = ImagePicker::CURRENT_VERSION
 	 */
 	public function __construct ($version = ImagePicker::CURRENT_VERSION) {
 		$this->_version = $version;
@@ -41,13 +39,6 @@ class ImagePickerJS implements IQuarkViewResource, IQuarkForeignViewResource, IQ
 	 */
 	public function Location () {
 		return 'https://cdnjs.cloudflare.com/ajax/libs/image-picker/' . $this->_version . '/image-picker.min.js';
-	}
-
-	/**
-	 * @return QuarkDTO
-	 */
-	public function RequestDTO () {
-		// TODO: Implement RequestDTO() method.
 	}
 
 	/**
