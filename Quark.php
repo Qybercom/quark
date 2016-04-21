a663e6bb73c9bd5a36f2ef44e07c58f73f8d70ed
Alex Furnica <saniafd93@gmail.com>
2016-04-21 18:22:06 +0300

    upgraded QuarkFile::_followParent

--- Quark.php
+++ Quark.php
@@ -11820,12 +11820,12 @@ class QuarkFile implements IQuarkModel, IQuarkStrongModel, IQuarkLinkedModel {
 	private function _followParent ($mode = self::MODE_DEFAULT) {
 		if (is_dir($this->parent) || is_file($this->parent)) return true;
 
-		$mask = umask(0);
-		$ok = @mkdir($this->parent, self::MODE_ANYONE/*$mode == self::MODE_DEFAULT ? $mask : $mode*/, true);
-		umask($mask);
+		//$mask = umask(0);
+		$ok = @mkdir($this->parent, self::MODE_GROUP/*$mode == self::MODE_DEFAULT ? $mask : $mode*/, true);
+		//umask($mask);
 		
-		if ($mask != umask())
-			Quark::Log('[QuarkFile::_followParent] The [umask:' . $mask . '] did not restored correctly for "' . $this->parent . '". Current: ' . umask());
+		//if ($mask != umask())
+			//Quark::Log('[QuarkFile::_followParent] The [umask:' . $mask . '] did not restored correctly for "' . $this->parent . '". Current: ' . umask());
 
 		if (!$ok)
 			Quark::Log('[QuarkFile::_followParent] Can not create dir "' . $this->parent . '". Error: ' . QuarkException::LastError());
