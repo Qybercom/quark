6a110180915ceff370a9ce6f0d7b80fa7d284ab7
Alex Furnica <saniafd93@gmail.com>
2016-05-26 20:13:13 +0300

    upgrade QuarkCollection and GoogleMap components

--- ViewResources/Google/GoogleMap.php
+++ ViewResources/Google/GoogleMap.php
@@ -31,6 +31,12 @@ class GoogleMap implements IQuarkViewResource, IQuarkLocalViewResource, IQuarkVi
 	const TYPE_SATELLITE = 'GoogleMap.Type.Satellite';
 	const TYPE_TERRAIN = 'GoogleMap.Type.Terrain';
 	const TYPE_HYBRID = 'GoogleMap.Type.Hybrid';
+	
+	const DEFAULT_ZOOM = 8;
+	const DEFAULT_WIDTH = 320;
+	const DEFAULT_HEIGHT = 240;
+	const DEFAULT_SCALE = 1;
+	const DEFAULT_SENSOR = false;
 
 	const GEOCODE_STATUS_OK = 'OK';
 	const GEOCODE_STATUS_ZERO_RESULTS = 'ZERO_RESULTS';
@@ -40,14 +46,14 @@ class GoogleMap implements IQuarkViewResource, IQuarkLocalViewResource, IQuarkVi
 	const GEOCODE_STATUS_UNKNOWN_ERROR = 'UNKNOWN_ERROR';
 
 	/**
-	 * @var string $_center = '0.0-0.0'
+	 * @var MapPoint $_center
 	 */
-	private $_center = '0.0-0.0';
+	private $_center;
 
 	/**
-	 * @var int $_zoom = 8
+	 * @var int $_zoom = self::DEFAULT_ZOOM
 	 */
-	private $_zoom = 8;
+	private $_zoom = self::DEFAULT_ZOOM;
 
 	/**
 	 * @var string $_size = '160x90'
@@ -55,9 +61,9 @@ class GoogleMap implements IQuarkViewResource, IQuarkLocalViewResource, IQuarkVi
 	private $_size = '160x90';
 
 	/**
-	 * @var int $_scale = 1
+	 * @var int $_scale = self::DEFAULT_SCALE
 	 */
-	private $_scale = 1;
+	private $_scale = self::DEFAULT_SCALE;
 
 	/**
 	 * @var string $_format = self::FORMAT_PNG
@@ -70,14 +76,14 @@ class GoogleMap implements IQuarkViewResource, IQuarkLocalViewResource, IQuarkVi
 	private $_type = self::TYPE_ROADMAP;
 
 	/**
-	 * @var string $_markers = ''
+	 * @var MapMarker[] $_markers = []
 	 */
-	private $_markers = '';
+	private $_markers = array();
 
 	/**
-	 * @var string $_paths = ''
+	 * @var MapPath[] $_paths = []
 	 */
-	private $_paths = '';
+	private $_paths = array();
 
 	/**
 	 * @var bool $_sensor = false
@@ -129,85 +135,88 @@ class GoogleMap implements IQuarkViewResource, IQuarkLocalViewResource, IQuarkVi
 	}
 
 	/**
-	 * @param float $lat
-	 * @param float $lng
+	 * @param MapPoint $center = null
 	 *
-	 * @return GoogleMap
+	 * @return MapPoint
 	 */
-	public function CenterByCoordinates ($lat, $lng) {
-		$this->_center = ((float)$lat) . '-' . ((float)$lng);
-		return $this;
+	public function Center (MapPoint $center = null) {
+		if (func_num_args() != 0)
+			$this->_center = $center;
+
+		return $this->_center;
 	}
 
 	/**
-	 * @param string $location
+	 * @param int $zoom = self::DEFAULT_ZOOM
 	 *
-	 * @return GoogleMap
+	 * @return int
 	 */
-	public function CenterByLocation ($location) {
-		$this->_center = (string)$location;
-		return $this;
+	public function Zoom ($zoom = self::DEFAULT_ZOOM) {
+		if (func_num_args() != 0)
+			$this->_zoom = (int)$zoom;
+
+		return $this->_zoom;
 	}
 
 	/**
-	 * @param int $zoom
+	 * @param int $width = self::DEFAULT_WIDTH
+	 * @param int $height = self::DEFAULT_HEIGHT
 	 *
-	 * @return GoogleMap
+	 * @return string
 	 */
-	public function Zoom ($zoom) {
-		$this->_zoom = (int)$zoom;
-		return $this;
+	public function Size ($width = self::DEFAULT_WIDTH, $height = self::DEFAULT_HEIGHT) {
+		if (func_num_args() != 0)
+			$this->_size = ((int)$width) . 'x' . ((int)$height);
+
+		return $this->_size;
 	}
 
 	/**
-	 * @param int $width
-	 * @param int $height
+	 * @param int $scale = self::DEFAULT_SCALE
 	 *
-	 * @return GoogleMap
+	 * @return int
 	 */
-	public function Size ($width, $height) {
-		$this->_size = ((int)$width) . 'x' . ((int)$height);
-		return $this;
+	public function Scale ($scale = self::DEFAULT_SCALE) {
+		if (func_num_args() != 0)
+			$this->_scale = (int)$scale;
+
+		return $this->_scale;
 	}
 
 	/**
-	 * @param int $scale
+	 * @param string $format = self::FORMAT_PNG
 	 *
-	 * @return GoogleMap
+	 * @return string
 	 */
-	public function Scale ($scale) {
-		$this->_scale = (int)$scale;
-		return $this;
+	public function Format ($format = self::FORMAT_PNG) {
+		if (func_num_args() != 0)
+			$this->_format = (string)$format;
+
+		return $this->_format;
 	}
 
 	/**
-	 * @param string $format
+	 * @param string $type = self::TYPE_ROADMAP
 	 *
-	 * @return GoogleMap
+	 * @return string
 	 */
-	public function Format ($format) {
-		$this->_format = (string)$format;
-		return $this;
+	public function MapType ($type = self::TYPE_ROADMAP) {
+		if (func_num_args() != 0)
+			$this->_type = (string)$type;
+
+		return $this->_type;
 	}
 
 	/**
-	 * @param string $type
+	 * @param bool $sensor = self::DEFAULT_SENSOR
 	 *
-	 * @return GoogleMap
+	 * @return bool
 	 */
-	public function MapType ($type) {
-		$this->_type = (string)$type;
-		return $this;
-	}
+	public function Sensor ($sensor = self::DEFAULT_SENSOR) {
+		if (func_num_args() != 0)
+			$this->_sensor = (bool)$sensor;
 
-	/**
-	 * @param bool $sensor
-	 *
-	 * @return GoogleMap
-	 */
-	public function Sensor ($sensor) {
-		$this->_sensor = (bool)$sensor;
-		return $this;
+		return $this->_sensor;
 	}
 
 	/**
@@ -216,8 +225,15 @@ class GoogleMap implements IQuarkViewResource, IQuarkLocalViewResource, IQuarkVi
 	 * @return GoogleMap
 	 */
 	public function Marker (MapMarker $marker) {
-		$this->_markers .= $marker->Compile();
+		$this->_markers[] = $marker;
 		return $this;
+	}
+
+	/**
+	 * @return MapMarker[]
+	 */
+	public function Markers () {
+		return $this->_markers;
 	}
 
 	/**
@@ -226,25 +242,40 @@ class GoogleMap implements IQuarkViewResource, IQuarkLocalViewResource, IQuarkVi
 	 * @return GoogleMap
 	 */
 	public function Path (MapPath $path) {
-		$this->_paths .= $path->Compile();
+		$this->_paths[] = $path;
 		return $this;
+	}
+
+	/**
+	 * @return MapPath[]
+	 */
+	public function Paths () {
+		return $this->_paths;
 	}
 
 	/**
 	 * @return string
 	 */
 	public function Image () {
-		return urlencode(
-			'http://maps.googleapis.com/maps/api/staticmap'
-			. '?center=' . $this->_center
+		$markers = '';
+		foreach ($this->_markers as $marker)
+			$markers .= $marker->Compile();
+
+		$paths = '';
+		foreach ($this->_paths as $path)
+			$paths .= $path->Compile();
+
+		return 'http://maps.googleapis.com/maps/api/staticmap'
+			. '?format=' . $this->_format
 			. '&zoom=' . $this->_zoom
 			. '&size=' . $this->_size
 			. '&scale=' . $this->_scale
-			. '&format=' . $this->_format
 			. '&maptype=' . $this->_type
 			. '&sensor=' . ($this->_sensor ? 'true' : 'false')
-			. (strlen($this->_key) != 0 ? '&key=' . $this->_key : '')
-		);
+			. ($this->_center != null ? '&center=' . $this->_center->Compile() : '')
+			. (sizeof($this->_markers) != 0 ? $markers : '')
+			. (sizeof($this->_paths) != 0 ? $paths : '')
+			. (strlen($this->_key) != 0 ? '&key=' . $this->_key : '');
 	}
 
 	/**
