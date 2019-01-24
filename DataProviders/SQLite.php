51949dda5b1c84a4d2a3b8c275364c1ad9668bb0
Alex Furnica <saniafd93@gmail.com>
2019-01-24 15:45:03 +0200

    added SQLite

--- DataProviders/SQLite.php
+++ DataProviders/SQLite.php
@@ -5,8 +5,12 @@ use Quark\IQuarkDataProvider;
 use Quark\IQuarkSQLDataProvider;
 use Quark\IQuarkModel;
 
+use Quark\Quark;
+use Quark\QuarkConnectionException;
 use Quark\QuarkField;
 use Quark\QuarkKeyValuePair;
+use Quark\QuarkModel;
+use Quark\QuarkSQL;
 use Quark\QuarkURI;
 
 /**
@@ -15,40 +19,91 @@ use Quark\QuarkURI;
  * @package Quark\DataProviders
  */
 class SQLite implements IQuarkDataProvider, IQuarkSQLDataProvider {
+	const PARAM_KEY = 'key';
+
+	/**
+	 * @var \SQLite3 $_connection
+	 */
+	private $_connection;
+
+	/**
+	 * @var QuarkSQL $_sql
+	 */
+	private $_sql;
+
+	/**
+	 * @var QuarkURI $_uri
+	 */
+	private $_uri;
+
 	/**
 	 * @param QuarkURI $uri
 	 *
 	 * @return mixed
+	 *
+	 * @throws QuarkConnectionException
 	 */
 	public function Connect (QuarkURI $uri) {
-		// TODO: Implement Connect() method.
+		$this->_uri = $uri;
+
+		if (!class_exists('\\SQLite3'))
+			throw new QuarkConnectionException($uri, Quark::LOG_FATAL, '[SQLite] Connection error: this PHP installation does not have configured SQLite extension');
+
+		try {
+			$this->_connection = new \SQLite3($uri->path, SQLITE3_OPEN_READWRITE|SQLITE3_OPEN_CREATE, $uri->Options(self::PARAM_KEY));
+
+			$this->_sql = new QuarkSQL($this);
+		}
+		catch (\Exception $e) {
+			throw new QuarkConnectionException($uri, Quark::LOG_FATAL, '[SQLite] ' . $e->getMessage());
+		}
 	}
 
 	/**
 	 * @param IQuarkModel $model
+	 * @param array $options
 	 *
 	 * @return mixed
 	 */
-	public function Create (IQuarkModel $model) {
-		// TODO: Implement Create() method.
+	public function Create (IQuarkModel $model, $options = []) {
+		$out = $this->_sql->Insert($model, $options);
+
+		$pk = $this->PrimaryKey($model)->Key();
+		$model->$pk = $this->_connection->lastInsertRowID();
+
+		return $out;
 	}
 
 	/**
 	 * @param IQuarkModel $model
+	 * @param array $options
 	 *
 	 * @return mixed
 	 */
-	public function Save (IQuarkModel $model) {
-		// TODO: Implement Save() method.
+	public function Save (IQuarkModel $model, $options = []) {
+		$pk = $this->PrimaryKey($model)->Key();
+
+		if (!isset($model->$pk)) return false;
+
+		return $this->Update($model, array(
+			$pk => $model->$pk
+		), $options);
 	}
 
 	/**
 	 * @param IQuarkModel $model
+	 * @param array $options
 	 *
 	 * @return mixed
 	 */
-	public function Remove (IQuarkModel $model) {
-		// TODO: Implement Remove() method.
+	public function Remove (IQuarkModel $model, $options = []) {
+		$pk = $this->PrimaryKey($model)->Key();
+
+		if (!isset($model->$pk)) return false;
+
+		return $this->Delete($model, array(
+			$pk => $model->$pk
+		), $options);
 	}
 
 	/**
@@ -57,75 +112,97 @@ class SQLite implements IQuarkDataProvider, IQuarkSQLDataProvider {
 	 * @return QuarkKeyValuePair
 	 */
 	public function PrimaryKey (IQuarkModel $model) {
-		// TODO: Implement PrimaryKey() method.
+		return $this->_sql->Pk($model, 'id', 0);
 	}
 
 	/**
 	 * @param IQuarkModel $model
-	 * @param             $criteria
-	 * @param             $options
+	 * @param $criteria
+	 * @param $options
 	 *
 	 * @return array
 	 */
 	public function Find (IQuarkModel $model, $criteria, $options) {
-		// TODO: Implement Find() method.
+		$output = array();
+
+		/**
+		 * @var \SQLite3Result $result
+		 */
+		$result= $this->_sql->Select($model, $criteria, $options);
+
+		if ($result && $result->numColumns() != 0) {
+			while ($record = $result->fetchArray(SQLITE3_ASSOC))
+				$output[] = $record;
+		}
+
+		return $output;
 	}
 
 	/**
 	 * @param IQuarkModel $model
-	 * @param             $criteria
-	 * @param             $options
+	 * @param $criteria
+	 * @param $options
 	 *
 	 * @return mixed
 	 */
 	public function FindOne (IQuarkModel $model, $criteria, $options) {
-		// TODO: Implement FindOne() method.
+		$records = $this->Find($model, $criteria, array_merge($options, array(QuarkModel::OPTION_LIMIT => 1)));
+
+		return sizeof($records) == 0 ? null : $records[0];
 	}
 
 	/**
 	 * @param IQuarkModel $model
-	 * @param             $id
-	 * @param             $options
+	 * @param $id
+	 * @param $options
 	 *
 	 * @return mixed
 	 */
 	public function FindOneById (IQuarkModel $model, $id, $options) {
-		// TODO: Implement FindOneById() method.
+		return $this->FindOne($model, array(
+			$this->PrimaryKey($model)->Key() => $id
+		), $options);
 	}
 
 	/**
 	 * @param IQuarkModel $model
-	 * @param             $criteria
-	 * @param             $options
+	 * @param $criteria
+	 * @param $options
 	 *
 	 * @return mixed
 	 */
 	public function Update (IQuarkModel $model, $criteria, $options) {
-		// TODO: Implement Update() method.
+		return $this->_sql->Update($model, $criteria, $options);
 	}
 
 	/**
 	 * @param IQuarkModel $model
-	 * @param             $criteria
-	 * @param             $options
+	 * @param $criteria
+	 * @param $options
 	 *
 	 * @return mixed
 	 */
 	public function Delete (IQuarkModel $model, $criteria, $options) {
-		// TODO: Implement Delete() method.
+		return $this->_sql->Delete($model, $criteria, $options);
 	}
 
 	/**
 	 * @param IQuarkModel $model
-	 * @param             $criteria
-	 * @param             $limit
-	 * @param             $skip
-	 * @param             $options
+	 * @param $criteria
+	 * @param $limit
+	 * @param $skip
+	 * @param $options
 	 *
 	 * @return int
 	 */
 	public function Count (IQuarkModel $model, $criteria, $limit, $skip, $options) {
-		// TODO: Implement Count() method.
+		$result = $this->_sql->Count($model, $criteria, array_merge($options, array(
+			QuarkModel::OPTION_FIELDS => array(QuarkSQL::FIELD_COUNT_ALL),
+			QuarkModel::OPTION_SKIP => $skip,
+			QuarkModel::OPTION_LIMIT => $limit == 0 ? 1 : $limit
+		)));
+
+		return !$result ? 0 : (int)$result->fetchArray()[0];
 	}
 
 	/**
@@ -135,7 +212,30 @@ class SQLite implements IQuarkDataProvider, IQuarkSQLDataProvider {
 	 * @return mixed
 	 */
 	public function Query ($query, $options) {
-		// TODO: Implement Query() method.
+		$out = @$this->_connection->query($query);
+
+		if (!$out)
+			Quark::Log('[SQLite] Query error "' . $query . '". Error: ' . $this->_connection->lastErrorMsg());
+
+		return $out;
+	}
+
+	/**
+	 * @param string $name
+	 *
+	 * @return string
+	 */
+	public function EscapeCollection ($name) {
+		return '"' . $name . '"';
+	}
+
+	/**
+	 * @param string $field
+	 *
+	 * @return string
+	 */
+	public function EscapeField ($field) {
+		return '"' . $field . '"';
 	}
 
 	/**
@@ -143,34 +243,8 @@ class SQLite implements IQuarkDataProvider, IQuarkSQLDataProvider {
 	 *
 	 * @return string
 	 */
-	public function Escape ($value) {
-		// TODO: Implement Escape() method.
-	}
-
-	/**
-	 * @return string
-	 */
-	public function EscapeChar () {
-		// TODO: Implement EscapeChar() method.
-	}
-
-	/**
-	 * @param string $table
-	 *
-	 * @return QuarkField[]
-	 */
-	public function Schema ($table) {
-		// TODO: Implement Schema() method.
-	}
-
-	/**
-	 * @param IQuarkModel $model
-	 * @param array $options
-	 *
-	 * @return string
-	 */
-	public function GenerateSchema (IQuarkModel $model, $options) {
-		// TODO: Implement GenerateSchema() method.
+	public function EscapeValue ($value) {
+		return $value === null ? null : \SQLite3::escapeString($value);
 	}
 
 	/**
@@ -189,5 +263,24 @@ class SQLite implements IQuarkDataProvider, IQuarkSQLDataProvider {
 	 */
 	public function FieldTypeFromModel ($field) {
 		// TODO: Implement FieldTypeFromModel() method.
+	}
+
+	/**
+	 * @param string $table
+	 *
+	 * @return QuarkField[]
+	 */
+	public function Schema ($table) {
+		// TODO: Implement Schema() method.
+	}
+
+	/**
+	 * @param IQuarkModel $model
+	 * @param array $options = []
+	 *
+	 * @return mixed
+	 */
+	public function GenerateSchema (IQuarkModel $model, $options = []) {
+		// TODO: Implement GenerateSchema() method.
 	}
 }
